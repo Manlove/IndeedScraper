247430fbcba3d7d83cf9c6d386364cd6c21e496d
Manlove_Logan <manlove.logan@gmail.com>
2018-10-24 23:17:25 -0500

    wrapped database in a class. added indeed_jobs.db

--- webscrape.py
+++ webscrape.py
@@ -15,30 +15,33 @@ companyList = []
 for div in soup.findAll("div", {"class", "result"}):
     a = div.find("a")
     titleList.append(a.get('title'))
-    
+
     company = div.find("span", {"class", "company"})
     companyList.append(company.getText().strip())
-    
+
     location = div.find("div", {"class", "location"})
     if location == None:
         location = div.find("span", {"class", "location"})
     locationList.append(location.getText().strip())
 
-   
-db = sq.connect('indeed_jobs.db')
-#db.execute('CREATE TABLE IF NOT EXISTS jobs (title text, company text, location text, date_applied text)')
-db.execute('DROP TABLE IF EXISTS jobs')
-db.execute('CREATE TABLE jobs (title text, company text, location text)')
+class job_log():
+    def __init__(self):
+        self.conn = sq.connect('indeed_jobs.db')
+        self.curs = self.conn.cursor()
+        self.setup()
 
+    def setup(self):
+        self.curs.execute('CREATE TABLE IF NOT EXISTS jobs (title text, company text, location text, date_applied text)')
 
+    def insert(self, title, company, location, date_applied = None):
+        if not date_applied:
+            date_applied = ""
+        self.curs.execute("INSERT INTO jobs (title, company, location, date_applied) VALUES (?, ?, ?, ?)", (title, company, location, date_applied))
+
+log = job_log()
 for i in range(0, len(titleList)):
-    insert = (titleList[i], companyList[i], locationList[i])
-    db.execute("INSERT INTO jobs (title,company,location) VALUES (?, ?, ?)", insert)
-
+    log.insert(titleList[i], companyList[i], locationList[i])
     
-db.commit()
-a = db.execute('SELECT title FROM jobs')
+a = log.conn.execute('SELECT title FROM jobs')
 for b in a:
     print(b)
-    
-
