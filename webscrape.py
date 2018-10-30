fef2bfd3f52ee3ede46780ae9b9ede4b55ed06fb
Manlove_Logan <manlove.logan@gmail.com>
2018-10-29 22:45:12 -0500

    wrap scraper in class

--- webscrape.py
+++ webscrape.py
@@ -3,45 +3,59 @@ import sqlite3 as sq
 import requests as urlr
 from bs4 import BeautifulSoup as bs
 
-url = 'https://www.indeed.com/jobs?q=bioinformatics&sort=date'
-page = urlr.get(url)
-soup = bs(page.content, 'html.parser')
+class scraper():
+    def __init__(self):
+        self.indeed_log = job_log()
+        self.url = 'https://www.indeed.com/jobs?q=bioinformatics&sort=date'
+        self.getIndeedJobsListPage(self.url)
+        self.titles, self.locations, self.companies = self.parseJobsList(self.soup)
+        self.log_jobs(self.titles, self.locations, self.companies)
 
+    def getIndeedJobsListPage(self, url):
+        page = urlr.get(url)
+        self.soup = bs(page.content, 'html.parser')
 
-titleList = []
-locationList = []
-companyList = []
+    def parseJobsList(self, soup):
+        titleList = []
+        locationList = []
+        companyList = []
+        for div in soup.findAll("div", {"class", "result"}):
+            a = div.find("a")
+            titleList.append(a.get('title'))
 
-for div in soup.findAll("div", {"class", "result"}):
-    a = div.find("a")
-    titleList.append(a.get('title'))
+            company = div.find("span", {"class", "company"})
+            companyList.append(company.getText().strip())
 
-    company = div.find("span", {"class", "company"})
-    companyList.append(company.getText().strip())
+            location = div.find("div", {"class", "location"})
+            if location == None:
+                location = div.find("span", {"class", "location"})
+            locationList.append(location.getText().strip())
+        return titleList, locationList, companyList
 
-    location = div.find("div", {"class", "location"})
-    if location == None:
-        location = div.find("span", {"class", "location"})
-    locationList.append(location.getText().strip())
+    def log_jobs(self, titles, locations, companies):
+        for i in range(0, len(titles)):
+            self.indeed_log.insert(titles[i], companies[i], locations[i])
 
 class job_log():
     def __init__(self):
         self.conn = sq.connect('indeed_jobs.db')
         self.curs = self.conn.cursor()
+        self.ex = self.curs.execute
         self.setup()
 
     def setup(self):
-        self.curs.execute('CREATE TABLE IF NOT EXISTS jobs (title text, company text, location text, date_applied text)')
+        self.ex('CREATE TABLE IF NOT EXISTS jobs (title text, company text, location text, date_applied text)')
 
     def insert(self, title, company, location, date_applied = None):
         if not date_applied:
             date_applied = ""
-        self.curs.execute("INSERT INTO jobs (title, company, location, date_applied) VALUES (?, ?, ?, ?)", (title, company, location, date_applied))
+        self.ex("INSERT INTO jobs (title, company, location, date_applied) VALUES (?, ?, ?, ?)", (title, company, location, date_applied))
 
-log = job_log()
-for i in range(0, len(titleList)):
-    log.insert(titleList[i], companyList[i], locationList[i])
-    
-a = log.conn.execute('SELECT title FROM jobs')
+    def reset(self):
+        self.ex("DROP TABLE jobs")
+
+indeed = scraper()
+
+a = indeed.indeed_log.ex('SELECT title FROM jobs')
 for b in a:
     print(b)
